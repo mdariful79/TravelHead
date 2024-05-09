dd339a5fe105a259e12577ef6be1579bfc02cbc7
mdariful79 <mdariful057@gmail.com>
2024-05-09 12:43:16 +0600

    Hero&Camp feature added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 
 export default function Home() {
   return (
-   <h1 className="font-bold text-teal-500 text-2xl">
-    Hello Bangladesh
-    </h1>
+   <>
+   <Hero />
+   <Camp />
+   <Guide />
+   <Features />
+   <GetApp />
+   <Footer />
+   </>
   );
 }
